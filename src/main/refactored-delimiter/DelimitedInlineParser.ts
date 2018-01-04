333cf4971342e50aa8441bbb43410dba1fd72498
AnyhowStep <anyhowstep@hotmail.com>
2018-01-03 21:07:38 -0500

    WIP attempt to make DelimParser core

---
+++ src/main/refactored-delimiter/DelimitedInlineParser.ts
@@ -0,0 +1,26 @@
+import {RegexStream} from "../refactored-misc/RegexStream";
+import {Delimiter, DelimiterCollection} from "../refactored-misc/DelimiterCollection";
+
+export interface DelimiterInfo {
+    beforeIsPunctuation : boolean,
+    afterIsPunctuation : boolean,
+    leftFlanking : boolean,
+    rightFlanking : boolean,
+}
+
+export interface ParseArgs {
+    delimiters : DelimiterCollection;
+    openerFound : boolean;
+    opener : Delimiter|null;
+    closer : Delimiter|null;
+}
+
+export abstract class DelimitedInlineParser {
+    public abstract getDelimiterCharacterCodes () : number[];
+    public abstract advanceDelimiter (stream : RegexStream, delimiter : number) : void;
+    public abstract canOpen (info : DelimiterInfo, delimiter : number) : boolean;
+    public abstract canClose (info : DelimiterInfo, delimiter : number) : boolean;
+
+    public abstract getDelimiterContent (stream : RegexStream, delimiterStartPosition : number, delimiter : number) : string;
+    public abstract tryParse (args : ParseArgs, delimiter : number) : void;
+}
---
+++ src/main/refactored-delimiter/SmartQuoteParser.ts
@@ -0,0 +1,52 @@
+import {DelimitedInlineParser, DelimiterInfo, ParseArgs} from "./DelimitedInlineParser";
+import {RegexStream} from "../refactored-misc/RegexStream";
+
+var C_SINGLEQUOTE = 39;
+var C_DOUBLEQUOTE = 34;
+
+export class SmartQuoteParser extends DelimitedInlineParser {
+    public getDelimiterCharacterCodes () : number[] {
+        return [
+            C_SINGLEQUOTE,
+            C_DOUBLEQUOTE
+        ];
+    }
+    public advanceDelimiter (stream : RegexStream) : void {
+        ++stream.pos;
+    }
+    public canOpen (info : DelimiterInfo) : boolean {
+        return info.leftFlanking && !info.rightFlanking;
+    }
+    public canClose (info : DelimiterInfo) : boolean {
+        return info.rightFlanking;
+    }
+
+    public getDelimiterContent (stream : RegexStream, delimiterStartPosition : number, delimiter : number) : string {
+        if (delimiter == C_SINGLEQUOTE) {
+            return "\u2019";
+        } else {
+            return "\u201C";
+        }
+    }
+    public tryParse (args : ParseArgs, delimiter : number) : void {
+        if (delimiter == C_SINGLEQUOTE) {
+            args.closer.node.setString("\u2019");
+            if (args.openerFound) {
+                if (args.opener == null) {
+                    throw new Error("opener cannot be null");
+                }
+                args.opener.node.setString("\u2018");
+            }
+            args.closer = args.closer.next;
+        } else {
+            args.closer.node.setString("\u201D");
+            if (args.openerFound) {
+                if (args.opener == null) {
+                    throw new Error("opener cannot be null");
+                }
+                args.opener.node.setString("\u201C");
+            }
+            args.closer = args.closer.next;
+        }
+    }
+}
