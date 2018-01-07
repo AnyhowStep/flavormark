16fa45ef12ea4b212a13f942782ac3a25d721e0b
AnyhowStep <anyhowstep@hotmail.com>
2018-01-07 01:28:26 -0500

    Cleanup

--- dist/src/main/commonmark/inline/parser/SmartStringParser.js
+++ dist/src/main/commonmark/inline/parser/SmartStringParser.js
@@ -1,36 +1,41 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const InlineParser_1 = require("./../../../InlineParser");
-//import {Node} from "./Node";
+//TODO Consider factoring this out into ellipsis and dash parser
 class SmartStringParser extends InlineParser_1.InlineParser {
     // Parse a run of ordinary characters, or a single character with
     // a special meaning in markdown, as a plain string.
     parse(parser, block) {
-        var m;
-        if ((m = parser.match(/^\.\.\./))) {
-            block.appendChild(parser.text("\u2026"));
-            return true;
+        {
+            const m = parser.match(/^\.\.\./);
+            if (m != undefined) {
+                block.appendChild(parser.text("\u2026"));
+                return true;
+            }
         }
-        else if ((m = parser.match(/^--+/))) {
-            var enCount = 0;
-            var emCount = 0;
-            if (m.length % 3 === 0) {
-                emCount = m.length / 3;
+        {
+            const m = parser.match(/^--+/);
+            if (m != undefined) {
+                var enCount = 0;
+                var emCount = 0;
+                if (m.length % 3 === 0) {
+                    emCount = m.length / 3;
+                }
+                else if (m.length % 2 === 0) {
+                    enCount = m.length / 2;
+                }
+                else if (m.length % 3 === 2) {
+                    enCount = 1;
+                    emCount = (m.length - 2) / 3;
+                }
+                else {
+                    enCount = 2;
+                    emCount = (m.length - 4) / 3;
+                }
+                const text = "\u2014".repeat(emCount) + "\u2013".repeat(enCount);
+                block.appendChild(parser.text(text));
+                return true;
             }
-            else if (m.length % 2 === 0) {
-                enCount = m.length / 2;
-            }
-            else if (m.length % 3 === 2) {
-                enCount = 1;
-                emCount = (m.length - 2) / 3;
-            }
-            else {
-                enCount = 2;
-                emCount = (m.length - 4) / 3;
-            }
-            m = "\u2014".repeat(emCount) + "\u2013".repeat(enCount);
-            block.appendChild(parser.text(m));
-            return true;
         }
         return false;
     }
--- src/main/commonmark/inline/parser/SmartStringParser.ts
+++ src/main/commonmark/inline/parser/SmartStringParser.ts
@@ -1,33 +1,39 @@
 import {InlineParser} from "./../../../InlineParser";
 import {InlineContentParser} from "./../../../InlineContentParser";
 import {Node} from "./../../../Node";
-//import {Node} from "./Node";
 
+//TODO Consider factoring this out into ellipsis and dash parser
 export class SmartStringParser extends InlineParser {
     // Parse a run of ordinary characters, or a single character with
     // a special meaning in markdown, as a plain string.
     public parse (parser : InlineContentParser, block : Node) : boolean {
-        var m;
-        if ((m = parser.match(/^\.\.\./))) {
-            block.appendChild(parser.text("\u2026"));
-            return true;
-        } else if ((m = parser.match(/^--+/))) {
-            var enCount = 0;
-            var emCount = 0;
-            if (m.length % 3 === 0) { // If divisible by 3, use all em dashes
-                emCount = m.length / 3;
-            } else if (m.length % 2 === 0) { // If divisible by 2, use all en dashes
-                enCount = m.length / 2;
-            } else if (m.length % 3 === 2) { // If 2 extra dashes, use en dash for last 2; em dashes for rest
-                enCount = 1;
-                emCount = (m.length - 2) / 3;
-            } else { // Use en dashes for last 4 hyphens; em dashes for rest
-                enCount = 2;
-                emCount = (m.length - 4) / 3;
+        {
+            const m = parser.match(/^\.\.\./);
+            if (m != undefined) {
+                block.appendChild(parser.text("\u2026"));
+                return true;
             }
-            m = "\u2014".repeat(emCount) + "\u2013".repeat(enCount);
-            block.appendChild(parser.text(m));
-            return true;
+        }
+        {
+            const m = parser.match(/^--+/);
+            if (m != undefined) {
+                var enCount = 0;
+                var emCount = 0;
+                if (m.length % 3 === 0) { // If divisible by 3, use all em dashes
+                    emCount = m.length / 3;
+                } else if (m.length % 2 === 0) { // If divisible by 2, use all en dashes
+                    enCount = m.length / 2;
+                } else if (m.length % 3 === 2) { // If 2 extra dashes, use en dash for last 2; em dashes for rest
+                    enCount = 1;
+                    emCount = (m.length - 2) / 3;
+                } else { // Use en dashes for last 4 hyphens; em dashes for rest
+                    enCount = 2;
+                    emCount = (m.length - 4) / 3;
+                }
+                const text = "\u2014".repeat(emCount) + "\u2013".repeat(enCount);
+                block.appendChild(parser.text(text));
+                return true;
+            }
         }
         return false;
     }
